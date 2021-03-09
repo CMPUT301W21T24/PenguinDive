283267cad0ee35cb0219adbfdbdd91c4261e4c47
Ryan Brooks <rbrook1598@gmail.com>
2021-03-08 17:57:39 -0700

    initial construction of Experiment_Manager which holds experiments

--- TrialClasses/Experiment_Manager.java
+++ TrialClasses/Experiment_Manager.java
@@ -1,70 +1,26 @@
 package com.example.cmput301project;
 
 public class Experiment_Manager {
-
-	private int numCount, numNonNeg, numMeas, numBinomial;
-	protected String description, region;
-	private ArrayList<Count_Trial> cTrials;
-	private ArrayList<Binomial_Trial> bTrials;
-	private ArrayList<Measurement_Trial> mTrials;
-	private ArrayList<Non_Negative_Integer_Counts_Trial> nnTrials;
+	private ArrayList<Hashmap<int, Experiment>> experiments;
+	private int key;
 
 	public Experiment_Manager() {
-		this.numCount = 0;
-		this.numNonNeg = 0;
-		this.numMeas = 0;
-		this.numBinomial = 0;
+		this.key = 0;
+		Experiments = new ArrayList<>();
 	}
 
-	public void createNewCount(Count_Trial c) {
-		this.cTrials.add(c);
-		this.numCount++;
+	public void addExperiment(Experiment e) {
+		this.key++;
+		Hashmap<int, Experiment> h = new Hashmap<>();
+		h = {this.key, e};
+		Experiments.add(h);
 	}
 
-	public void createNewNonNeg(Non_Negative_Integer_Counts_Trial n) {
-		this.nnTrials.add(n);
-		this.numNonNeg++;
-	}
-
-	public void createNewMeasurement(Measurement_Trial m) {
-		this.mTrials.add(m);
-		this.numMeas++;
-	}
-
-	public void createNewBinomial(Binomial_Trial b) {
-		this.bTrials.add(b);
-		this.numBinomial++;
-	}
-
-	public void publish() {
-
-	}
-
-	public void unpublish() {
-
-	}
-
-	public String end() {
-		return;
-	}
-
-	public void upload() {
-
-	}
-
-	public void ignore() {
-
-	}
-
-	public void stats() {
-
-	}
-
-	public void histogram() {
-
-	}
-
-	public void plots() {
-		
+	public void delExperiment(int k) {
+		for (int i = 0; i < Experiments.size(); i++) {
+			if (Experiments.get(i).containsKey(k)) {
+				Experiments.get(i).remove();
+			}
+		}
 	}
 }
