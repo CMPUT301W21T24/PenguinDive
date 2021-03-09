e7bb77b6d3d24a1bcfc63c5b654467d9bfe98201
Tyler Bach <tpbach@ualberta.ca>
2021-03-09 14:04:13 -0700

    quick file update

--- app/src/main/java/com/cmput301/penguindive/MainActivity.java
+++ app/src/main/java/com/cmput301/penguindive/MainActivity.java
@@ -3,12 +3,49 @@ package com.cmput301.penguindive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+    private ListView list;
+    private SearchView SearchContent;
+    private ExampleAdapter adapter;
+    public static ArrayList<Example> exampleArrayList = new ArrayList<Example>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list.setAdapter(adapter);
+
+        Example e = new Example("user", "owner!", "this is a desc", "big title", "active");
+
+        list = (ListView) findViewById(R.id.listView);
+
+        exampleArrayList.add(e);
+
+        SearchContent = (SearchView) findViewById(R.id.searchLayout);
+        SearchContent.setOnQueryTextListener(this);
+
     }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
+    }
+
+
 }
--- app/src/main/java/com/cmput301/penguindive/SearchFragment.java
+++ app/src/main/java/com/cmput301/penguindive/SearchFragment.java
@@ -1,4 +1,5 @@
 package com.cmput301.penguindive;
 
 public class SearchFragment {
+    // To be discussed, searchview is more optimal
 }
