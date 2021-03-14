fc98b9aac4526aa0bbebb63f5aa0403e09fd43fb
Tyler Bach <tpbach@ualberta.ca>
2021-03-14 16:42:31 -0600

    Integrated experiment search functionality.

--- app/src/main/java/com/cmput301/penguindive/MainActivity.java
+++ app/src/main/java/com/cmput301/penguindive/MainActivity.java
@@ -3,37 +3,85 @@ package com.cmput301.penguindive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, ExperimentFragment.OnFragmentInteractionListener{
 
-    private ListView list;
+    private ListView experimentList;
     private SearchView SearchContent;
-    private ExampleAdapter adapter;
-    public static ArrayList<Example> exampleArrayList = new ArrayList<Example>();
-
+    private ExperimentCustomList experimentAdapter;
+    public static ArrayList<Experiment> experimentDataList = new ArrayList<Experiment>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.experiment_activity); //To be changed to main
 
-        list.setAdapter(adapter);
+        experimentList = findViewById(R.id.experimentList);
+        experimentDataList = new ArrayList<>();
 
-        Example e = new Example("user", "owner!", "this is a desc", "big title", "active");
+        experimentAdapter = new ExperimentCustomList(this);
+        experimentList.setAdapter(experimentAdapter);
 
-        list = (ListView) findViewById(R.id.listView);
+        final FloatingActionButton addButton = findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ExperimentFragment().show(getSupportFragmentManager(), "ADD");
+            }
+        });
 
-        exampleArrayList.add(e);
-
-        SearchContent = (SearchView) findViewById(R.id.searchLayout);
-        SearchContent.setOnQueryTextListener(this);
+        // create a clickListener on the existing experiment
+        experimentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Experiment trail = experimentDataList.get(position);
+                ExperimentFragment.newInstance(trail, position).show(getSupportFragmentManager(), "Edit");
+            }
+        });
 
     }
+
+    //changes after clicking OK/Edit/Delete button
+    @Override
+    public void onOkPressed(Experiment newExperiment) {
+        experimentDataList.add(newExperiment);
+        experimentAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onEditPressed(Experiment newExperiment, int position) {
+        experimentDataList.set(position,newExperiment);
+        experimentAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDeletePressed(Experiment experiment) {
+        experimentDataList.remove(experiment);
+        experimentAdapter.notifyDataSetChanged();
+    }
+
+    //showing message when there is any invalid input
+    @Override
+    public void extraStringError() {
+        Toast.makeText(MainActivity.this,"The description exceeds the maximum limitation.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void nullValueError() {
+        Toast.makeText(MainActivity.this,"The information of description and date of the experiment is required.", Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public boolean onQueryTextSubmit(String query) {
@@ -42,10 +90,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        String text = newText;
-        adapter.filter(text);
+        experimentAdapter.filter(newText);
         return false;
     }
-
-
 }
