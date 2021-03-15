73bd17cae95cae4ecd211ef3222c67c8d4c89321
tpbach <64627383+tpbach@users.noreply.github.com>
2021-03-14 18:08:01 -0600

    Revert "Revert "fixed previous commit not having proper changes made""
    This reverts commit e2039b2e304ec4fc3e378bc1592c23635db516d7.

--- app/src/main/java/com/cmput301/penguindive/Experiment.java
+++ app/src/main/java/com/cmput301/penguindive/Experiment.java
@@ -8,16 +8,24 @@ public class Experiment implements Serializable {
     private String region;
     private String totalTrail;
     private String ownerUserName;
-    public Experiment(String title, String description, String region, String totalTrail, String ownerUserName) {
+    private String status;
+
+    public Experiment(String title, String description, String region, String totalTrail, String ownerUserName, String status) {
         this.title = title;
         this.description = description;
         this.region = region;
         this.totalTrail = totalTrail;
         this.ownerUserName = ownerUserName;
+        this.status = status;
     }
     String getTitle(){return this.title;}
     String getDescription() {return this.description;}
     String getRegion() {return this.region;}
     String getTotalTrail() {return this.totalTrail;}
     String getOwnerUserName() {return this.ownerUserName;}
+    String getStatus() {return status;}
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
