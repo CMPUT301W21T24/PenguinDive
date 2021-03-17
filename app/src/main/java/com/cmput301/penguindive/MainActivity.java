442e693c2deb27f8438cdb1199e7deb91d396566
linrui06 <linrui@ualberta.ca>
2021-03-17 16:47:21 -0600

    simple welcome page and uid generator

--- app/src/main/java/com/cmput301/penguindive/MainActivity.java
+++ app/src/main/java/com/cmput301/penguindive/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +22,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -41,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements ExperimentFragmen
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference experimentCollectionReference = db.collection("Experiments");
     Button myExperiment;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
