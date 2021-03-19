ee4ea79cd323eb6dfc86f27f368b4a0cf9c389d4
Nikita <nikitaadekar1798@gmail.com>
2021-03-19 08:29:08 -0600

    added Question and Answer Activity

--- app/src/main/java/com/cmput301/penguindive/MainActivity.java
+++ app/src/main/java/com/cmput301/penguindive/MainActivity.java
@@ -14,12 +14,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, ExperimentFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity{
 
-    private ListView experimentList;
-    private SearchView SearchContent;
-    private ExperimentCustomList experimentAdapter;
-    public static ArrayList<Experiment> experimentDataList = new ArrayList<Experiment>();
 
 
     @Override
@@ -27,70 +23,9 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        experimentList = findViewById(R.id.experimentList);
-        experimentDataList = new ArrayList<>();
 
-        experimentAdapter = new ExperimentCustomList(this);
-        experimentList.setAdapter(experimentAdapter);
-
-        final FloatingActionButton addButton = findViewById(R.id.add_button);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new ExperimentFragment().show(getSupportFragmentManager(), "ADD");
-            }
-        });
-
-        // create a clickListener on the existing experiment
-        experimentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Experiment trail = experimentDataList.get(position);
-                ExperimentFragment.newInstance(trail, position).show(getSupportFragmentManager(), "Edit");
-            }
-        });
 
     }
 
-    //changes after clicking OK/Edit/Delete button
-    @Override
-    public void onOkPressed(Experiment newExperiment) {
-        experimentDataList.add(newExperiment);
-        experimentAdapter.notifyDataSetChanged();
-    }
 
-    @Override
-    public void onEditPressed(Experiment newExperiment, int position) {
-        experimentDataList.set(position,newExperiment);
-        experimentAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onDeletePressed(Experiment experiment) {
-        experimentDataList.remove(experiment);
-        experimentAdapter.notifyDataSetChanged();
-    }
-
-    //showing message when there is any invalid input
-    @Override
-    public void extraStringError() {
-        Toast.makeText(MainActivity.this,"The description exceeds the maximum limitation.", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void nullValueError() {
-        Toast.makeText(MainActivity.this,"The information of description and date of the experiment is required.", Toast.LENGTH_SHORT).show();
-    }
-
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        experimentAdapter.filter(newText);
-        return false;
-    }
 }
---
+++ app/src/main/java/com/cmput301/penguindive/QuestionCustomList.java
@@ -0,0 +1,43 @@
+package com.cmput301.penguindive;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+
+public class QuestionCustomList extends ArrayAdapter<Question> {
+
+    private ArrayList<Question> questionArrayList;
+    private Context context;
+    private int resource;
+
+    public QuestionCustomList( Context context, int resource, ArrayList<Question> questionArrayList) {
+        super(context, resource, questionArrayList);
+        this.questionArrayList = questionArrayList;
+        this.context = context;
+        this.resource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+
+        if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.question_content,parent,false);
+        }
+
+        Question question = questionArrayList.get(position);
+        TextView questionText = view.findViewById(R.id.question_text);
+        questionText.setText(question.getQuestion());
+        return view;
+    }
+}
