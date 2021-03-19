13118e763082f487282dd01e28a9a278d0fa4314
rbrooks1 <77315138+rbrooks1@users.noreply.github.com>
2021-03-19 15:01:24 -0600

    added comment on functionality

--- app/src/main/java/com/cmput301/penguindive/QRScanner.java
+++ app/src/main/java/com/cmput301/penguindive/QRScanner.java
@@ -37,6 +37,7 @@ Created by: bikashthapa01: https://github.com/bikashthapa01
 Licence: unknown (could not find)
 URL: https://github.com/bikashthapa01/QR-APP/tree/master/app
 
+This class allows users to scan a printed QR code and will read out the text of the QR code back to the user
  */
 public class QRScanner extends AppCompatActivity {
 
