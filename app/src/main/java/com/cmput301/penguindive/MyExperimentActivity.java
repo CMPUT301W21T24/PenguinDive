073029f3dc2e4165547699b89cc10e59da323c54
tpbach <64627383+tpbach@users.noreply.github.com>
2021-04-08 18:43:32 -0600

    Added javadocs for all navigation methods across all applicable activities

--- app/src/main/java/com/cmput301/penguindive/MyExperimentActivity.java
+++ app/src/main/java/com/cmput301/penguindive/MyExperimentActivity.java
@@ -299,31 +299,82 @@ public class MyExperimentActivity extends AppCompatActivity implements Experimen
         return keywords;
     }
 
-    // Refresh method
+    /**
+     * This method refreshes the current activity
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickRefresh(View view){
         MainActivity.redirectActivity(this, MyExperimentActivity.class);
     }
 
+    /**
+     * This method gives the current drawer layout to the openDrawer method in MainActivity
+     * It is called when the hamburger icon is clicked on the toolbar
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickMenu(View view){ MainActivity.openDrawer(drawerLayout);}
 
+    /**
+     * This method gives the current drawer layout to the closeDrawer method in MainActivity
+     * It is called when the PenguinDive logo is clicked in the drawer
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickLogo(View view){ MainActivity.closeDrawer(drawerLayout);}
 
+    /**
+     * This method redirects the user to MainActivity
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickHome(View view){MainActivity.redirectActivity(this,MainActivity.class); }
 
+    /**
+     * This method redirects the user to the MyExperimentActivity
+     * Since the user is already on MyExperimentActivity, it simply closes the drawer
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickMyExperiments(View view){ MainActivity.closeDrawer(drawerLayout); }
 
+    /**
+     * This method redirects the user to the PickScanType Activity
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickScanQrCode(View view){ MainActivity.redirectActivity(this,PickScanType.class); }
 
+    /**
+     * This method redirects the user to the PickQRType Activity
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickGenerateQrCode(View view){ MainActivity.redirectActivity(this,PickQRType.class); }
 
+    /**
+     * This method redirects the user to the their profile page (Profile Activity)
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickMyProfile(View view){
         MainActivity.redirectActivity(this,Profile.class);
     }
 
+    /**
+     * This method redirects the user to the search users page (SearchProfile Activity)
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickSearchUsers(View view){ MainActivity.redirectActivity(this,SearchProfile.class); }
 
+    /**
+     * This method calls openGitHub in MainActivity and provides it the current activity
+     * @param view
+     * Takes a view representing the current view
+     */
     public void ClickGitHub(View view){
         MainActivity.openGitHub(this);
     }
-
 }
